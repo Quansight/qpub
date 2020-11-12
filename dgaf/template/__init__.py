487a1d2a882ce206b51c5e55d9cbd64b9b9853b6
tonyfast <tony.fast@gmail.com>
2020-11-12 17:56:03 -0500

    +

--- dgaf/template/__init__.py
+++ dgaf/template/__init__.py
@@ -1,4 +1,4 @@
 import dgaf
 
 pyproject = (dgaf.File(__file__).parent / "pyproject.toml").load()
-_config = dgaf.File(__file__).parent / "_config.yml").load()
+_config = (dgaf.File(__file__).parent / "_config.yml").load()
