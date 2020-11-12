d653d9bcfe1618b950b1605c1cc4db8fdb7418e8
tonyfast <tony.fast@gmail.com>
2020-11-12 17:34:35 -0500

    console script

--- dgaf/__main__.py
+++ dgaf/__main__.py
@@ -2,5 +2,10 @@ import doit
 import sys
 from . import dodo
 
-doit.doit_cmd.DoitMain(doit.cmd_base.ModuleTaskLoader(
-    vars(dodo))).run(sys.argv[1:])
+
+def main():
+    doit.doit_cmd.DoitMain(doit.cmd_base.ModuleTaskLoader(
+        vars(dodo))).run(sys.argv[1:])
+
+
+main()
