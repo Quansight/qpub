9fbc1de6024cec0d2803278bb3014255bfd57d4c
tonyfast <tony.fast@gmail.com>
2020-11-15 09:29:08 -0500

    add templates

--- dgaf/template/__init__.py
+++ dgaf/template/__init__.py
@@ -1,4 +1,5 @@
 import dgaf
 
-pyproject = (dgaf.File(__file__).parent / "pyproject.toml").load()
+flit = (dgaf.File(__file__).parent / "pyproject.toml").load()
+poetry = (dgaf.File(__file__).parent / "pyproject_poetry.toml").load()
 _config = (dgaf.File(__file__).parent / "_config.yml").load()
