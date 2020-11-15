ce7295c7cf3665a8608bd16363b4d8faf80377f6
tonyfast <tony.fast@gmail.com>
2020-11-15 17:44:10 -0500

    defer to typer

--- dgaf/__init__.py
+++ dgaf/__init__.py
@@ -4,9 +4,12 @@ from . import util
 from .util import File, merge, Module, Path
 from . import template
 
+
 def main():
     with __import__("tingle").Markdown():
         from . import readme
 
     import sys
+
+    readme.app()
     util.typer_to_doit(readme.app).run(sys.argv[1:])
