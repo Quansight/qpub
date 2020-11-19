e44ba2d0531f9299d438b74bbfe0177c948608b7
tonyfast <tony.fast@gmail.com>
2020-11-19 17:16:53 -0500

    drop test for click since we move to doit

--- dgaf/tests/test_dgaf.py
+++ dgaf/tests/test_dgaf.py
@@ -1,5 +1,4 @@
 import doit
-import typer
 import dgaf
 
 
@@ -23,13 +22,3 @@ def test_merge():
     assert dgaf.merge(dict(b=dict(a=[1])), dict(b=dict(a=[2]))) == dict(
         b=dict(a=[1, 2])
     )
-
-
-def test_cli(cli_runner):
-    import dgaf
-
-    __import__("tingle").loaders.XO.extensions += [".md"]
-    with __import__("tingle").loaders.XO():
-        import dgaf.readme
-
-    cli_runner.invoke(typer.main.get_command(dgaf.app), "--help".split())
