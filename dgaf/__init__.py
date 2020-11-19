8aefbb53659feede134154d1ad8dd45d6ca1a300
tonyfast <tony.fast@gmail.com>
2020-11-19 16:05:23 -0500

    remove typer and deploy cli with doit

--- dgaf/__init__.py
+++ dgaf/__init__.py
@@ -1,13 +1,13 @@
 """deathbeds generalized automation framework."""
 __version__ = __import__("datetime").date.today().strftime("%Y.%m.%d")
-import typer
 
-app = typer.Typer()
 from . import util
 from .util import File, merge, Module, Path, task
 from . import template, files, converters
 
 __import__("xonsh.main").main.setup()
+
+
 def main():
     from . import tasks, docs
 
