9b5c47656932f253793a41dfb8869f5ec6ad6fed
tonyfast <tony.fast@gmail.com>
2020-11-19 16:33:48 -0500

    use python for dodo instead of literate code.

--- dodo.py
+++ dodo.py
@@ -1,2 +1,28 @@
-with __import__("tingle").Markdown():
-    from readme import *
+"""tasks to reproduce the environment and development environment."""
+
+
+def task_dev():
+
+    """install dgaf in development mode."""
+    return dict(
+        actions="""
+pip install -rrequirements.txt
+python -m dgaf infer develop
+    """.strip().splitlines(),
+        file_dep=["requirements.txt"],
+        uptodate=[False],
+    )
+
+
+def task_setup():
+
+    """install the built dgaf this is used in [github actions] for testing this package on mac, windows, and linux."""
+
+    return dict(
+        actions="""
+pip install -rrequirements.txt
+python -m dgaf infer setup
+    """.strip().splitlines(),
+        file_dep=["requirements.txt"],
+        uptodate=[False],
+    )
