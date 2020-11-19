ba7a2de3330cb6b97b7acaa1efb157e37affc128
tonyfast <tony.fast@gmail.com>
2020-11-19 16:27:43 -0500

    flag files to avoid acyclic graphs

--- dgaf/files.py
+++ dgaf/files.py
@@ -5,6 +5,10 @@ import os
 import git
 
 
+class Flagged(File):
+    ...
+
+
 CONDA = bool(os.getenv("CONDA_EXE"))
 CONDA_ENV = os.getenv("CONDA_DEFAULT_ENV")
 CONDA_EXE = os.getenv("CONDA_EXE")
@@ -14,26 +18,34 @@ TOC = File("_toc.yml") or File("docs/_toc.yml")
 DOCS = File("docs")  # a convention with precedence from github
 DOIT_CFG = File(".doit.cfg")
 ENV = dgaf.util.Dict()
-ENVIRONMENT = File("environment.yaml") or File("environment.yml")
-CONTENT = FILES = [
-    x
-    for x in (File(x) for x in git.Git().ls_files().splitlines())
-    if x not in (File("postBuild"),)
-]
+ENVIRONMENT = Flagged("environment.yaml") or Flagged("environment.yml")
+
 GITHUB = File(".github")
-GITIGNORE = File(".gitignore")
+GITIGNORE = Flagged(".gitignore")
 INDEX = File("index.html")
-POSTBUILD = File("postBuild")
-PYPROJECT = File("pyproject.toml")
+POSTBUILD = Flagged("postBuild")
+PYPROJECT = Flagged("pyproject.toml")
 README = File("readme.md")
 REPO = git.Repo()
-REQUIREMENTS = File("requirements.txt")
-SETUPPY = File("setup.py")
-SETUPCFG = File("setup.cfg")
+REQUIREMENTS = Flagged("requirements.txt")
+SETUPPY = Flagged("setup.py")
+SETUPCFG = Flagged("setup.cfg")
 SUBMODULES = [File(x.path) for x in REPO.submodules]
 TOX = File("tox.ini")
 
 WORKFLOWS = GITHUB / "workflows"
+
+
+IGNORED = dgaf.merge(dgaf.template.gitignore, GITIGNORE.load())
+INCLUDE = [File(x.lstrip("!")) for x in IGNORED if x.startswith("!")]
+
+OS = os.name
+
+BUILT_SPHINX = File("_build/sphinx")
+FLAGGED = [x for x in locals().values() if isinstance(x, Flagged)]
+CONTENT = FILES = [
+    x for x in (File(x) for x in git.Git().ls_files().splitlines()) if x not in FLAGGED
+]
 DIRECTORIES = list(
     set(
         x.parent
@@ -44,10 +56,3 @@ DIRECTORIES = list(
     )
 )
 TOP_LEVEL = [x for x in DIRECTORIES if x.parent == File()]
-
-IGNORED = dgaf.merge(dgaf.template.gitignore, GITIGNORE.load())
-INCLUDE = [File(x.lstrip("!")) for x in IGNORED if x.startswith("!")]
-
-OS = os.name
-
-BUILT_SPHINX = File("_build/sphinx")
