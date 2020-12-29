7588fa27fc01774652da5586cafb2af3e057d7c5
tonyfast <tony.fast@gmail.com>
2020-12-29 08:50:50 -0500

    update tasks

--- dodo.py
+++ dodo.py
@@ -1,11 +1,28 @@
+# ██████╗  ██████╗ ██╗████████╗    ████████╗ █████╗ ███████╗██╗  ██╗███████╗
+# ██╔══██╗██╔═══██╗██║╚══██╔══╝    ╚══██╔══╝██╔══██╗██╔════╝██║ ██╔╝██╔════╝
+# ██║  ██║██║   ██║██║   ██║          ██║   ███████║███████╗█████╔╝ ███████╗
+# ██║  ██║██║   ██║██║   ██║          ██║   ██╔══██║╚════██║██╔═██╗ ╚════██║
+# ██████╔╝╚██████╔╝██║   ██║          ██║   ██║  ██║███████║██║  ██╗███████║
+# ╚═════╝  ╚═════╝ ╚═╝   ╚═╝          ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+
+
 def task_python_gitignore():
     """download the gitignore file for excluding python content."""
     import pathlib
 
+    dgaf = pathlib.Path("dgaf")
+    targets = [
+        dgaf / "templates" / "Python.gitignore",
+        dgaf / "templates" / "Nikola.gitignore",
+        dgaf / "templates" / "JupyterNotebooks.gitignore",
+    ]
+
     return dict(
         actions=[
-            """wget https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -O dgaf/Python.gitignore"""
+            """wget https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -O dgaf/templates/Python.gitignore""",
+            """wget https://raw.githubusercontent.com/github/gitignore/master/community/Python/Nikola.gitignore -O dgaf/templates/Nikola.gitignore""",
+            """wget https://raw.githubusercontent.com/github/gitignore/master/community/Python/ .gitignore -O dgaf/templates/JupyterNotebooks.gitignore""",
         ],
-        targets=["dgaf/Python.gitignore"],
-        uptodate=[pathlib.Path("dgaf/Python.gitignore").exists()],
+        targets=targets,
+        uptodate=list(map(pathlib.Path.exists, targets)),
     )
